f200bc7c46c19d34e03cf04137aca52eee343f9c
Christopher Kaster <me@atomicptr.de>
2024-10-07 15:04:51 +0200

    update dune

--- pkgs/dune-developer-preview/default.nix
+++ pkgs/dune-developer-preview/default.nix
@@ -1,15 +1,19 @@
-{ lib, fetchurl, stdenvNoCC }:
+{
+  lib,
+  fetchurl,
+  stdenvNoCC,
+}:
 let
   # this has to be updated manually
-  date = "2024-10-04";
+  date = "2024-10-07";
   # use this to regenerate:
   #    curl -sSf -o /tmp/dune "https://get.dune.build/$(date +%Y-%m-%d)/x86_64-unknown-linux-musl/dune" && sha256sum /tmp/dune && rm /tmp/dune
-  hash = "0566acaf39cf225428883944b0a66e10e4bb907d8b83a53bd8f6c3365e2b0436";
+  hash = "b633e910a68bd832103ebd69419755caecab206e864c8435498352d43583af50";
   arch = "x86_64-unknown-linux-musl";
 in
 stdenvNoCC.mkDerivation {
   pname = "dune-developer-preview";
-  version = builtins.replaceStrings ["-"] ["."] date;
+  version = builtins.replaceStrings [ "-" ] [ "." ] date;
   src = fetchurl {
     url = "https://get.dune.build/${date}/${arch}/dune";
     sha256 = hash;
