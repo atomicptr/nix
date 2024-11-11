940e63f0496c4ed4931e2547e93eea850d2656cd
Christopher Kaster <me@atomicptr.de>
2024-11-11 10:58:09 +0100

    update dune dev preview to 2024.11.08

--- pkgs/dune-developer-preview/default.nix
+++ pkgs/dune-developer-preview/default.nix
@@ -5,10 +5,10 @@
 }:
 let
   # this has to be updated manually
-  date = "2024-11-03";
+  date = "2024-11-08";
   # use this to regenerate:
   #    export DUNE_VERSION="2024-11-03" && curl -sSf -o /tmp/dune "https://get.dune.build/$DUNE_VERSION/x86_64-unknown-linux-musl/dune" && sha256sum /tmp/dune && rm /tmp/dune
-  hash = "74bfc562af483d158ba8dac61b217b3edeef02670b9dc6188d572c8badeea580";
+  hash = "441c593ccd4cbb72c13fc7f3fb53ac575fe4e52533f7c8ad553dd630b5bd3204";
   arch = "x86_64-unknown-linux-musl";
 in
 stdenvNoCC.mkDerivation {
