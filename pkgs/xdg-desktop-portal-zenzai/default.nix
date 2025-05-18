bb26d973967ad0573aed8942b3fef42fa92463dd
Christopher Kaster <me@atomicptr.de>
2025-05-18 13:04:23 +0200

    init xdg-desktop-portal-zenzai at 0.1.0

---
+++ pkgs/xdg-desktop-portal-zenzai/default.nix
@@ -0,0 +1,46 @@
+{
+  cargo,
+  fetchFromGitHub,
+  lib,
+  meson,
+  ninja,
+  rustPlatform,
+  rustc,
+  stdenv,
+  xdg-desktop-portal,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "xdg-desktop-portal-zenzai";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "atomicptr";
+    repo = "xdg-desktop-portal-zenzai";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-uBcpW890JgYNc+KB2Ze9FDn9DSdvb61QltatgC32nBs=";
+  };
+
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-IWxTQCs6cWzqnJPwNY9HwIkC+KidtGYWUA0+Iq0KbJE=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    rustc
+    rustPlatform.cargoSetupHook
+    cargo
+    ninja
+  ];
+
+  buildInputs = [
+    xdg-desktop-portal
+  ];
+
+  meta = {
+    description = "A collection of several xdg-desktop-portal implementations to serve more lightweight wayland compositors like Hyprland";
+    homepage = "https://github.com/atomicptr/xdg-desktop-portal-zenzai";
+    license = lib.licenses.gpl3;
+    platforms = lib.platforms.linux;
+  };
+})
