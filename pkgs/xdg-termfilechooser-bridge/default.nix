f208df033d62c3efe19c2bad78b8623855aec988
Christopher Kaster <me@atomicptr.de>
2025-08-13 14:57:48 +0200

    update xdg-termfilechooser-bridge to 0.2.5

--- pkgs/xdg-termfilechooser-bridge/default.nix
+++ pkgs/xdg-termfilechooser-bridge/default.nix
@@ -8,18 +8,18 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-termfilechooser-bridge";
-  version = "0.2.4";
+  version = "0.2.5";
 
   src = fetchFromGitHub {
     owner = "atomicptr";
     repo = "xdg-termfilechooser-bridge";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-FnILhD7fWOjPNewputJlJlPRe2Ls9cQMzcPnfpLB3a8=";
+    hash = "sha256-sR0ZRpUCXan/PXdfVwhestJuKPy3Jcz0rXwOCl8gUMc=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-+F1OZjuE5pJlcUEloKc5Iu29VyzhZxz/072zP8wGY7o=";
+    hash = "sha256-qdJiW91ya/FDu49MrRg7FJIGI4LDOnbdjF8MGVbRHI8=";
   };
 
   nativeBuildInputs = [
